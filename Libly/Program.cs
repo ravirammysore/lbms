126a3637e4420a48db9b5bd14a3a015aa8253239
Ravi K R <raviram.mysore@gmail.com>
2024-09-11 12:43:26 +0530

    enabled razor pages and added index page

--- Libly/Program.cs
+++ Libly/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//adds Razor Pages services to the DI container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Ravi!");
+//config that u do here (settings) are called Middleware
+//app.MapGet("/", () => "Welcome to Libly!");
+
+/*
+ * UseRouting means that we won't be hardcoding every route, rather
+ * the framework should figure it out dynamically based on conventions
+ */
+
+//object.function()
+app.UseRouting();
+/*
+ *  MapRazorPages() tells the app to look for Razor Pages in the Pages folder 
+ *  and handle requests accordingly. Other option is MVC style controllers
+ */
+app.MapRazorPages();
 
 app.Run();
