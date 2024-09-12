735dfcc76bf9d956fbae3a80b8618ce53612ac3f
Ravi K R <raviram.mysore@gmail.com>
2024-09-12 10:54:29 +0530

    now displaying booknames

--- Libly/Pages/Books/Index.cshtml.cs
+++ Libly/Pages/Books/Index.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages; //external package (lib)
 
-namespace Libly.Pages.Books
+namespace Libly.Pages.Books // namespace (folder)
 {
-    public class IndexModel : PageModel
+    public class IndexModel : PageModel //class  //PascalCase
     {
-        public void OnGet()
+        public string[] bookNames; //camel
+        public void OnGet() //function
         {
-        }
+            //eventually i will pull this from database
+            bookNames = [
+              "The Great Gatsby",
+                "To Kill a Mockingbird",
+                "The God Father",
+                "Pride and Prejudice"
+          ];
+        }      
     }
 }
