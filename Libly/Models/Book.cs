388d730dc19f9b226e4df9912a4804964d69c607
Ravi K R <raviram.mysore@gmail.com>
2024-09-13 11:20:39 +0530

    Refactoring - Fix the code in the page UI to now use properties

---
+++ Libly/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Libly.Models
+{
+    public class Book
+    {
+        //attributes ----> properties
+        public int Id { get; set; } //auto-implemented property
+        public string Title { get; set; } //let us not have method just for getting and setting
+        public string Category { get; set; } //CamelCase
+        public DateTime Dop { get; set; }
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? ModifiedOn { get; set; }
+
+        public Book(int id, string title, string category, DateTime dop)
+        {
+            Id = id;
+            Title = title;
+            Category = category;
+            Dop = dop;
+            CreatedOn = DateTime.Now;
+        }
+    }
+}
--- Libly/Pages/Books/Index.cshtml.cs
+++ Libly/Pages/Books/Index.cshtml.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages; 
-
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Libly.Models;
 namespace Libly.Pages.Books 
 {
     public class IndexModel : PageModel 
@@ -19,84 +19,5 @@ namespace Libly.Pages.Books
                 new Book(4, "Pride and Prejudice", "Romance", new DateTime(1813, 1, 28))
             };
         }      
-    }
-
-    public class Book
-    {
-        private int id;
-        private string title;
-        private string category;
-        private DateTime dop;
-        private DateTime createdOn;
-        private DateTime? modifiedOn;
-
-        //a ctor is a special function
-        //has name, no return type, invoked auto
-        
-        //ensure that devs create an obj the way it has to be
-        public Book(int id, string title, string category, DateTime dop)
-        {
-            this.id = id;
-            this.title = title;
-            this.category = category;
-            this.dop = dop;
-            this.createdOn = DateTime.Now;
-        }
-
-        //getters and setters (accessors and mutators)
-        public int GetId()
-        {
-            return id;
-        }
-   
-        public void SetId(int value)
-        {
-            id = value;
-        }
-
-        public string GetTitle()
-        {
-            return title;
-        }
-
-        public void SetTitle(string value)
-        {
-            title = value;
-        }
-
-        public string GetCategory()
-        {
-            return category;
-        }
-
-        public void SetCategory(string value)
-        {
-            category = value;
-        }
-
-        public DateTime GetDop()
-        {
-            return dop;
-        }
-
-        public void SetDop(DateTime value)
-        {
-            dop = value;
-        }
-
-        public DateTime GetCreatedOn()
-        {
-            return createdOn;
-        }
-
-        public DateTime? GetModifiedOn()
-        {
-            return modifiedOn;
-        }
-
-        public void SetModifiedOn(DateTime? value)
-        {
-            modifiedOn = value;
-        }
-    }
+    }    
 }
