89bae929324e6db20127fdc1ebc16f6965bb78a5
Ravi K R <raviram.mysore@gmail.com>
2024-09-16 12:43:04 +0530

    Made the code more robust

--- Libly/Models/Book.cs
+++ Libly/Models/Book.cs
@@ -1,4 +1,6 @@
-﻿namespace Libly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libly.Models
 {
     public class Book
     {
@@ -6,6 +8,8 @@
         public int Id { get; set; } //auto-implemented property
         public string Title { get; set; } //let us not have method just for getting and setting
         public string Category { get; set; } //CamelCase
+
+        [DataType(DataType.Date)]
         public DateTime Dop { get; set; }
         public DateTime CreatedOn { get; private set; }
         public DateTime? ModifiedOn { get; set; } //nullables (do not know it)
