183494bca537939b68619afa0084edf32d8a8c5a
Ravi K R <raviram.mysore@gmail.com>
2024-09-18 11:18:35 +0530

    Create function is working with some design flaw

--- Libly/Pages/Books/Create.cshtml.cs
+++ Libly/Pages/Books/Create.cshtml.cs
@@ -2,29 +2,53 @@ using Libly.Data;
 using Libly.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Libly.Pages.Books
 {
     public class CreateModel : PageModel
     {
         [BindProperty] //Devs had to manually process the body
-        public Book Book { get; set; }      
-       
+        public Book Book { get; set; }
+
+        public List<SelectListItem> CategoryOptions { get; set; }
+
         public void OnGet()
         {
-
+            PopulateDropdown();
         }
+
         //This is also invoked auto when the server receives a POST request from the client
         public ActionResult OnPost()
         {
             //Validate the book 
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
+                PopulateDropdown();
                 return Page();
             }
             BooksData.Create(Book); //upto the data-service 
 
             return RedirectToPage("./Index");
+        }
+
+        private void PopulateDropdown()
+        {
+            //Object Initialization Syntax
+            CategoryOptions = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Value = "1",
+                    Text = "Fiction"
+                },
+
+                new SelectListItem
+                {
+                    Value = "2",
+                    Text = "Science Fiction"
+                }
+            };
         }
     }
 }
