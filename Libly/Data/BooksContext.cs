4b1db850f69eaaa6c06704c3cf72e14cb30ff695
Ravi K R <raviram.mysore@gmail.com>
2024-09-20 11:40:04 +0530

    Modify the Index page model to fetch the data using EF

--- Libly/Data/BooksContext.cs
+++ Libly/Data/BooksContext.cs
@@ -6,8 +6,18 @@ namespace Libly.Data
     //An object of this will be used to fetch or save entities from the DB
     public class BooksContext : DbContext
     {
+        string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=LibraryDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+        
         //This is an in-memory collection of the data in Books table
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
     }
 }
--- Libly/Pages/Books/Index.cshtml.cs
+++ Libly/Pages/Books/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Libly.Models;
 using Libly.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Libly.Pages.Books 
 {
@@ -10,7 +11,10 @@ namespace Libly.Pages.Books
         public List<Book> books; //Collections
         public void OnGet() 
         {
-            books = BooksData.GetAll(); //fetch this stuff froma real db or a service later!
+            //books = BooksData.GetAll(); //fetch this stuff froma real db or a service later!
+
+            var context = new BooksContext();
+            books = context.Books.Include(b=>b.Category).ToList();
         }      
     }    
 }
