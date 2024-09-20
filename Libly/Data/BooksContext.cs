5b3f258e5d19123dfaa936a7975892a82e095d6f
Ravi K R <raviram.mysore@gmail.com>
2024-09-20 11:11:12 +0530

    Configure Entity framework

---
+++ Libly/Data/BooksContext.cs
@@ -0,0 +1,13 @@
+﻿using Libly.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libly.Data
+{
+    //An object of this will be used to fetch or save entities from the DB
+    public class BooksContext : DbContext
+    {
+        //This is an in-memory collection of the data in Books table
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
