b0a2ef44468458a84aab629661c6f5046066511f
Ravi K R <raviram.mysore@gmail.com>
2024-09-20 12:41:46 +0530

    Delete is using EF

--- Libly/Pages/Books/Delete.cshtml.cs
+++ Libly/Pages/Books/Delete.cshtml.cs
@@ -2,6 +2,7 @@ using Libly.Data;
 using Libly.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Libly.Pages.Books
 {
@@ -12,7 +13,12 @@ namespace Libly.Pages.Books
         public ActionResult OnGet(int id)
         {
             // Retrieve the book to be deleted
-            Book = BooksData.Get(id);
+            //Book = BooksData.Get(id);
+            
+            var context = new BooksContext();
+            Book = context.Books
+                .Include(b=>b.Category)
+                .SingleOrDefault(b=>b.Id == id);    
 
             if (Book == null)
             {
@@ -25,7 +31,14 @@ namespace Libly.Pages.Books
         public ActionResult OnPost(int id)
         {
             //ask the service code to delete this
-            BooksData.Delete(id);   
+            //BooksData.Delete(id);   
+
+            var context = new BooksContext();
+            //Removes it from DbSet only
+            context.Books.Remove(Book);
+            
+            //never call this in a loop
+            context.SaveChanges();
 
             //return back to index
             return RedirectToPage("./Index");    
--- Libly/Pages/Books/Index.cshtml.cs
+++ Libly/Pages/Books/Index.cshtml.cs
@@ -14,7 +14,15 @@ namespace Libly.Pages.Books
             //books = BooksData.GetAll(); //fetch this stuff froma real db or a service later!
 
             var context = new BooksContext();
-            books = context.Books.Include(b=>b.Category).ToList();
+
+            //Explicit loading (we specify which related entities we need)
+            books = context.Books
+                .Include(b=>b.Category)                                
+                .ToList();
+
+            //The navigation property (related entity) is NOT loaded untill you ask for it!
+            //In EF classic this would still work out of the box!
+            books = context.Books.ToList();
         }      
     }    
 }
