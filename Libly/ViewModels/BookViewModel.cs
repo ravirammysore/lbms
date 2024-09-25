e5e433b76570e6787a2d28faee6c8496056dfe44
Ravi K R <raviram.mysore@gmail.com>
2024-09-25 11:03:58 +0530

    added validation error messages

--- Libly/ViewModels/BookViewModel.cs
+++ Libly/ViewModels/BookViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Libly.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libly.ViewModels
 {
     public class BookViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Book titles are mandatory for adding a new book!"), Length(1, 30)]        
         public string Title { get; set; }
         public DateTime Dop { get; set; }
         public int CategoryId { get; set; }
