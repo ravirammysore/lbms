bac71283bf82013a95f31eb2812130df960a2ecf
Ravi K R <raviram.mysore@gmail.com>
2024-09-30 11:28:03 +0530

    Now returning an in-memory book list

--- Libly.API/Program.cs
+++ Libly.API/Program.cs
@@ -10,5 +10,24 @@ app.UseHttpsRedirection();
 
 app.MapGet("/api/libly", () =>  "Libly App");
 
+//functional programing 
+app.MapGet("/api/books", () =>
+{
+    var books = new List<Book>
+    {
+        new Book { Id = 1, Title = "The Great Gatsby", Dop = new DateTime(1925, 4, 10) },
+        new Book { Id = 2, Title = "To Kill a Mockingbird", Dop = new DateTime(1960, 7, 11) },
+        new Book { Id = 3, Title = "1984", Dop = new DateTime(1949, 6, 8) }
+    };
+
+    return books;
+});
+
 app.Run();
 
+class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Dop { get; set; }
+}
